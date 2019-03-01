ca7f94013a46ae9e760004fbd61156b629443d87
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 19:16:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function createPost() {
+  const pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  const postTemplate = _.template(document.getElementById("post-template").innerHTML);
+}
+
+ function postComment() {
+   const commentsTemplate = document.getElementById("comments-template");
+   const commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+   }
